bada51cc37b915e0a50c9160072fed8e405b0a28
Mauricio Cordeiro <cordmaur@gmail.com>
2022-09-29 12:29:59 +0200

    PIP Installation

--- waterquality/__init__.py
+++ waterquality/__init__.py
@@ -4,45 +4,46 @@ from waterquality.WaterQuality import DWWaterQuality
 import os
 from pathlib import Path
 
-__version__ = '1.1.3'
+__version__ = "1.1.4"
 
-print(f'Initializing waterquality package (version={__version__})')
+print(f"Initializing waterquality package (version={__version__})")
 
 # try to find waterdetect
 try:
     import waterdetect
-    print(f'Waterdetect version: {waterdetect.__version__}')
+
+    print(f"Waterdetect version: {waterdetect.__version__}")
 except Exception as e:
-    print('Waterdetect not found')
+    print("Waterdetect not found")
     print(e)
 
 
 def correct_proj_path():
-    '''
+    """
     Correct the path for the proj.db in environment variables
-    '''
+    """
 
-    print('Trying to locate the proj.db inside GDAL.')
+    print("Trying to locate the proj.db inside GDAL.")
 
     # get the gdal path
-    gdal_path = Path(os.environ['GDAL_DATA'])
+    gdal_path = Path(os.environ["GDAL_DATA"])
 
     # test proj inside gdal
-    proj_path = gdal_path/'proj'
-    if not (proj_path/'proj.db').exists():
-        proj_path = gdal_path.parent/'proj'
+    proj_path = gdal_path / "proj"
+    if not (proj_path / "proj.db").exists():
+        proj_path = gdal_path.parent / "proj"
 
-        if not (proj_path/'proj.db').exists():
-            print(f'Could not find proj.db')
+        if not (proj_path / "proj.db").exists():
+            print(f"Could not find proj.db")
             return
-    
-    os.environ['PROJ_LIB'] = proj_path.as_posix()
 
-# correct the GDAL environment
-if 'PROJ_LIB' in os.environ:
-    proj_path = Path(os.environ['PROJ_LIB'])
-    if not proj_path.exists():
-        correct_proj_path()
-else:
-    correct_proj_path()
-    
+    os.environ["PROJ_LIB"] = proj_path.as_posix()
+
+
+# # correct the GDAL environment
+# if "PROJ_LIB" in os.environ:
+#     proj_path = Path(os.environ["PROJ_LIB"])
+#     if not proj_path.exists():
+#         correct_proj_path()
+# else:
+#     correct_proj_path()
